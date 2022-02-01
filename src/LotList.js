7216f57a96b73e82230220bd37062c7f9955efff
jprudolph03 <jprudolph03@gmail.com>
2022-02-01 18:45:19 -0500

    adding math to table

--- src/LotList.js
+++ src/LotList.js
@@ -2,11 +2,15 @@ import React from "react";
 
 function LotList(props) {
   let cList = props.lots;
+  console.log(cList);
   const makeList = cList.map((i) => (
     <tr key={i._id}>
       <th>{i.num}</th>
-      <th>{i.totalCoilWeight}</th>
-      <th>{i.STDLoss}</th>
+      <th>
+        {i.totalCoilWeight}
+        <strong> ({i.totalCoilWeight - i.STDLoss})</strong>
+      </th>
+      <th>{((i.totalCoilWeight - i.STDLoss) / i.singlePartWeight) * 1000}</th>
       <th>{i.singlePartWeight}</th>
     </tr>
   ));
@@ -17,7 +21,7 @@ function LotList(props) {
           <tr>
             <th scope="col">#</th>
             <th scope="col">Total Coil Weight</th>
-            <th scope="col">STD Loss</th>
+            <th scope="col">Expected</th>
             <th scope="col">SP Weight</th>
           </tr>
         </thead>
